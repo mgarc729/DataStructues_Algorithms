47e1d3cb626083447e9c18b411ba584ecaf91e5e
Manuel Garcia Cruz <manuelgarciacruz@Manuels-MacBook-Pro.local>
2017-11-08 23:19:30 -0500

    Created skeleton for a binary search tree

---
+++ DataStructures/BinaryTree.py
@@ -0,0 +1,31 @@
+class BinaryNode(object):
+	def __init__(self, val):
+		self.val = val
+		self.left = None
+		self.right = None
+
+
+
+class BinaryTree(object):
+	def __init__(self):
+		pass
+
+	def insert(self, val):
+		pass
+
+	def delete(self, val):
+		pass
+
+	def search(self, val):
+		pass
+
+	def in_order_traversal(self):
+		pass
+
+	def pre_order_traversal(self):
+		pass
+
+	def post_order_traversal(self):
+		pass
+
+	
--- DataStructures/Queue.py
+++ DataStructures/Queue.py
@@ -1,15 +1,87 @@
 from LinkedList import LinkedList
 
+
 class Queue(object):
+	"""Basic implementation of a Queue structure
 
+		Attributes:
+			queue_list: holds the LinkedList used to implement the Queue
+
+	"""
 	def __init__(self):
-		self.queue = LinkedList()
+		"""Default Constructor
 
+			It creates an empty queue.
+		"""
+		self.queue_list = LinkedList()
 
+	def add(self, value):
+		"""Adds an element to the queue."""
+		self.queue_list.insert(value)
 
+	def peek(self):
+		"""Returns the first element in the queue"""
+		if self.is_empty():
+			return None
 
+		return self.queue_list.head.val
 
+	def remove(self):
+		"""Removes the next element in the queue and returns its value."""
+		if self.is_empty():
+			raise Exception("Queue is empty")
+
+		return self.queue_list.delete(0)
+
+	def is_empty(self):
+		"""Checks whether the queue is empty or not"""
+		return self.queue_list is None
+
+	def __str__(self):
+		"""Specifies how to print the queue
+
+			The printing of the queue looks like:
+			=( a b c d )=>
+
+			where a,b,c,d are elements in the queue and "d" is the first in the queue.
+		"""
+		output = ""
+
+		current = self.queue_list.head
+
+		while current is not None:
+			output = str(current.val) + " " + output
+
+			current = current.next
+
+		output = "=( " + output + ")=>"
+		return output
 
 
 if __name__ == '__main__':
+	# Some test cases
+
+	# Queue Creation
 	queue = Queue()
+
+	# Inserting some elements to the queue
+	queue.add(1)
+	queue.add(2)
+	queue.add(3)
+	queue.add(4)
+	queue.add(5)
+	queue.add(6)
+
+	# Check that the elements has been inserted
+	print queue
+
+	# Check peek method
+	print queue.peek()
+
+	# Check remove method
+	print queue.remove()
+
+	print queue
+
+
+
